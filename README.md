e68a908e1de82a6a84009160662965209cef6b00
curder <q.curder@gmail.com>
2022-02-12 17:49:10 +0800

    Init Project

---
+++ README.md
@@ -0,0 +1,3 @@
+## What's New in Laravel
+
+- [What's New in Laravel 9](/docs/9.x/README.md)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    base: "/what-is-new-in-laravel/",
+    lang: 'zh-CN',
+    title: 'What\'s New in Laravel',
+    description: '记录 Laravel 发布稳定版本更新的新特性',
+    themeConfig: {
+        navbar: [
+            {text: '首页', link: '/'},
+            {text: 'Laravel 9.x', link: '/9.x/'},
+        ],
+        sidebar: {
+            //
+        },
+    }
+}
