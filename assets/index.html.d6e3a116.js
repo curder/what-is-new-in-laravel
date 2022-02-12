71e97bfc83a465546d70ecb4f8e4c3fbd2de675f
curder <curder@users.noreply.github.com>
2022-02-12 10:43:40 +0000

    Add github workflows build-docs 806d5ef91ebb471be9998499bab4334f03d70bff

---
+++ assets/index.html.d6e3a116.js
@@ -0,0 +1,11 @@
+import{r as t,o as c,c as r,w as e,a as o,b as n,d as s}from"./app.ed330300.js";import{_ as i}from"./plugin-vue_export-helper.21dcd24c.js";const p={},u=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token comment"},"# install in your project"),s(`
+
+`),n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token function"},"watch"),s(),n("span",{class:"token comment"},"# start writing"),s(`
+
+`),n("span",{class:"token function"},"yarn"),s(" prod "),n("span",{class:"token comment"},"# build to static files"),s(`
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br")])],-1),m=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"npm"),s(),n("span",{class:"token function"},"install"),s(),n("span",{class:"token comment"},"# install in your project"),s(`
+
+npx `),n("span",{class:"token function"},"watch"),s(),n("span",{class:"token comment"},"# start writing"),s(`
+
+npx prod `),n("span",{class:"token comment"},"# build to static files"),s(`
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br")])],-1);function b(d,_){const a=t("CodeGroupItem"),l=t("CodeGroup");return c(),r(l,null,{default:e(()=>[o(a,{title:"YARN",active:""},{default:e(()=>[u]),_:1}),o(a,{title:"NPM"},{default:e(()=>[m]),_:1})]),_:1})}var h=i(p,[["render",b]]);export{h as default};
