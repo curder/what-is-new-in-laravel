75cdfff8144767a69b3092117c0a219d31b52502
curder <q.curder@gmail.com>
2022-02-14 13:54:11 +0800

    Add some theme config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,24 @@ module.exports = {
     title: 'What\'s New in Laravel',
     description: '记录 Laravel 发布稳定版本更新的新特性',
     themeConfig: {
+        editLinks: true,
+        docsBranch: 'master',
+        docsDir: 'docs',
+        editLinkPattern: ':repo/-/edit/:branch/:path',
+        lastUpdated: true,
+        lastUpdatedText: '最后更新时间',
+        contributors: true,
+        contributorsText: '贡献者列表',
         navbar: [
-            {text: '首页', link: '/'},
-            {text: 'Laravel 8.x', link: '/8.x'},
-            {text: 'Laravel 9.x', link: '/9.x'},
+            {text: 'Home', link: '/'},
+            {
+                text: "Version",
+                children: [
+                    {text: 'Laravel 8.x', link: '/8.x'},
+                    {text: 'Laravel 9.x', link: '/9.x'},
+                ],
+            },
+
         ],
         sidebar: {
             //
